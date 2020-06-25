a32e41ff50dbf0842f8c8dc4e8eaca706ea0331e
Marcelo Hexsel <marcelo.hexsel@hotmail.com>
2020-06-25 19:47:25 -0300

    Criando componente de Card conforme html do bootstrap

---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card c-card">
+    <img :src="image" class="card-img-top" alt="" />
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <p class="card-text">
+        {{ text }}
+      </p>
+      <a href="#" class="btn btn-primary" @click="buttonClick">{{
+        buttonText
+      }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    buttonText: {
+      type: String,
+    },
+  },
+  methods: {
+    buttonClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.c-card {
+  width: 100%;
+  height: 100%;
+}
+
+.card-img-top {
+  width: 100%;
+  height: 250px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="home">
     <h1>Vue CSS</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-3">
+          <card
+            image="https://img-a.udemycdn.com/course/750x422/1917274_4784.jpg"
+            title="VueJS"
+            text="A progressive and reactive awesome front-end framework"
+            button-text="Learn"
+            @click="vueClick"
+          ></card>
+        </div>
+        <div class="col-3">
+          <card
+            image="https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/Iq6QSY8vRs6XAaOJ10UP"
+            title="React"
+            text="Facebook main front-end tecnology"
+            button-text="Learn"
+            @click="reactClick"
+          ></card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from "../components/Card";
+
 export default {
+  components: {
+    Card,
+  },
   name: "Home",
+  methods: {
+    vueClick() {
+      alert("learn vuejs!");
+    },
+    reactClick() {
+      alert("learn react!");
+    },
+  },
 };
 </script>
